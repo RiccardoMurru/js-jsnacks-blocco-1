90cccd084157585265c3463abea20a4333dcfdc8
Riccardo Murru <riccardo.mr@gmail.com>
2020-04-03 15:16:59 +0200

    add. Jsnack 1

---
+++ js/jsnack1.js
@@ -0,0 +1,34 @@
+/**
+ * Snack 1: 
+ *  L’utente inserisce due parole in successione, con due prompt. 
+ *  Il software stampa la parola più lunga. 
+ *  Utilizza una funzione per determinare e ritornare la parola più lunga.
+ */
+
+ var firstWord = prompt('Inserisci la prima parola');
+ var secondWord = prompt('Inserisci la seconda parola');
+ var result = wordLength(firstWord, secondWord);
+
+ // stampa la parola più lunga
+ if (result == 1) {
+    console.log('La parola più lunga è', firstWord);
+ } else if (result == 2) {
+    console.log('La parola più lunga è', secondWord);
+ }  else {
+     console.log('Le parole sono di uguale lunghezza');
+     
+ }
+
+ // funzione per determinare e restituire la parola più lunga
+function wordLength(word1, word2) {
+    var res;
+    if (word1.length > word2.length) {
+        res = 1;
+    } else if (word1.length < word2.length) {
+        res = 2;
+    }   else {
+        res = 0;
+    }
+
+    return res;
+}
